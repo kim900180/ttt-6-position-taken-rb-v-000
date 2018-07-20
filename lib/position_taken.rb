b3d8bad0a6357e91145dff208a5cf97bf836ee1a
Jae Yong Kim <kim900180@gmail.com>
2018-07-20 20:25:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board(index) == "" || board(index) == " "
     false
-  elsif board(index) != "" || board(index) == " "
+  elsif board(index) "X" || board(index) == "O"
     true
   end
 end
